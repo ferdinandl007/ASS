97e190cd5e15cefd636a95c7763f69909f1f1292
Ali Azam <ali.azam@kcl.ac.uk>
2017-04-02 03:52:02 +0100

    added postdata

--- routes/index.js
+++ routes/index.js
@@ -7,21 +7,58 @@ var Location = require('../Model/Location');
 
 var watson = require('watson-developer-cloud');
 var fs = require('fs');
+var multipart = require('connect-multiparty');
+var multipartMiddleware = multipart();
+
+var multipartstream = require('multipart-read-stream');
+var pump = require('pump');
+var http = require('http');
+
 
 var visual_recognition = watson.visual_recognition({
-    api_key: '9be213bceee345ed33febf2124ed5a920db946d6',
+    api_key: '577febf5f2455d14c15923bd5a7d0d3d5af3aa5b',
     version: 'v3',
     version_date: '2016-05-20'
 });
 
 var params = {
-    images_file: fs.createReadStream( __dirname +"/scene00001.jpg"),
-    classifier_ids: ['person1_2003340273']
+    url : '',
+    classifier_ids: ['person3_1788983704']
 };
 
 
+router.post('/postdata', function (req, res) {
 
 
+    var latitude = req.body.latitude;
+    var longitude = req.body.longitude;
+    var url = req.body.url;
+
+
+    params.url = url;
+
+
+    sendToWatson(function (flag) {
+
+        if(flag) {
+
+            var messsage = 'Please rescue at ' + 'logitiude: ' + longitude + ' \n' + 'latituide: ' + latitude + '\n' + 'Thanks';
+
+            recievers.forEach(function (pn) {
+                console.log('pn is:', pn);
+                //twillio.sendSms(pn, messsage);
+            })
+            res.send('All Done');
+
+        } else {
+            res.send('Nothing to be done');
+
+        }
+
+    });
+
+
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -77,4 +114,39 @@ function sendToWatson(callback){
 
 }
 
+
+
+router.post('/postimage1', multipartMiddleware, function (req, res) {
+
+    //params.images_file = req.files.file;
+
+    //params.image = req.files;
+
+    console.log(req.files.file);
+
+    var multipartStream = multipart(req.headers, handler);
+
+    pump(req, multipartStream, function (err) {
+        if (err) res.end('server error')
+        res.end()
+    })
+
+    function handler (fieldname, file, filename) {
+        console.log('reading file ' + filename + ' from field ' + fieldname)
+        var fileStream = fs.createWriteStream(path.join('/tmp', filename))
+        pump(file, fileStream)
+
+
+    }
+
+
+
+  /*  sendToWatson(function (flag) {
+        console.log('some');
+
+    })*/
+    res.send('thanjs');
+
+
+})
 module.exports = router;
